99c5c8f671ba52b3443671367664c36f45e826ad
Cristopher Orellana <cristopherorellana2023@gmail.com>
2026-04-15 17:12:38 +0200

    metodos de fichero normal y ficher binario creados...

--- src/main/java/es/iesquevedo/dao/CrearAndLeerFichero.java
+++ src/main/java/es/iesquevedo/dao/CrearAndLeerFichero.java
@@ -8,7 +8,68 @@ import java.util.List;
 
 public class CrearAndLeerFichero {
 
+//ficheros cuando se cree un nuevo juego
+
     public static void guardar(ArrayList<Elemento> lista) {
+
+        try {
+            FileWriter fw = new FileWriter(Constantes.FICHERO);
+
+            for (Elemento e : lista) {
+                fw.write(e.toStringFichero() + "\n");
+            }
+
+            fw.close();
+            System.out.println(Constantes.FICHERO_GUARDADO);
+
+        } catch (IOException e) {
+            System.out.println(Constantes.ERROR_GUARDAR);
+        }
+    }
+
+    public static ArrayList<Elemento> leer() {
+
+        ArrayList<Elemento> lista = new ArrayList<>();
+        File fichero = new File(Constantes.FICHERO);
+
+        // Si no existe devolver la lista vacía
+        if (!fichero.exists()) {
+            return lista;
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fichero));
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+
+                // toString palabra ; categoria
+                String[] partes = linea.split(" ; ");
+
+                if (partes.length == 2) {
+                    String palabra = partes[0];
+                    String categoria = partes[1];
+
+                    lista.add(new Elemento(palabra, categoria));
+                }
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println(Constantes.ERROR_LEER);
+        }
+        return lista;
+    }
+
+
+
+
+
+
+    /// Ficheros binarios para cuando se elija vovler a cargar una partida anterior
+    public static void guardarFicheroBinario(ArrayList<Elemento> lista) {
                 try {
                     ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("palabras.txt"));
                     oos.writeObject(lista);
@@ -20,7 +81,7 @@ public class CrearAndLeerFichero {
         }
     }
 
-    public static List<Elemento> leer() {
+    public static List<Elemento> leerFicheroBinario() {
         List<Elemento> palabras = null;
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(Constantes.FICHERO))){
             palabras = (List<Elemento>) ois.readObject();
